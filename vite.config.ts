3f4ac03acca27e656ff0caf7ea939c298e729731
Khem Upreti <roadsidefc@gmail.com>
2025-04-09 14:10:05 +0545

    remove unused vars

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	server: {
 		port: 3000
-		// proxy: {
-		// 	'/api': 'https://north-case-api.vercel.app'
-		// }
-	},
-	define: {
-		__SVELTE_DEV__: true
 	},
 	plugins: [tailwindcss(), sveltekit()]
 });
